de96950ccc7471e5fd8bfa2e61385cfaa977fae3
Ted Bjurlin <tbjurlin@rocketsoftware.com>
2025-10-22 17:50:22 -0400

    Installed log4j

--- src/main/java/com/loggingprototype/Main.java
+++ src/main/java/com/loggingprototype/Main.java
@@ -1,7 +1,13 @@
 package com.loggingprototype;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    protected static final Logger logger = LogManager.getLogger();
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        logger.info("Hello log!");
     }
 }
