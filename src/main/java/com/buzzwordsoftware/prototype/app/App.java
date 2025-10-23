ccf64468ec31c526b7d7e250187b417bcf1d51f6
Ted Bjurlin <tbjurlin@rocketsoftware.com>
2025-10-23 10:56:06 -0400

    Finished base project

--- src/main/java/com/buzzwordsoftware/prototype/app/App.java
+++ src/main/java/com/buzzwordsoftware/prototype/app/App.java
@@ -1,10 +1,17 @@
 package com.buzzwordsoftware.prototype.app;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 /**
  * Hello world!
  */
 public class App {
+
+    protected static final Logger LOGGER = LogManager.getLogger();
+    
     public static void main(String[] args) {
+        LOGGER.info("Hello Log!");
         System.out.println("Hello World!");
     }
 }
