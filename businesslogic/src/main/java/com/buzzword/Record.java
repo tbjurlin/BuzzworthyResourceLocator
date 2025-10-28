d90916b0a6aba52b6848e4440d25045f34f7cb85
Dennis Shelby <dshelby@rocketsoftware.com>
2025-10-28 16:37:19 -0400

    Record entity class skeleton code.

---
+++ businesslogic/src/main/java/com/buzzword/Record.java
@@ -0,0 +1,61 @@
+public class Record {
+    private int id;
+    private int creatorId;
+    private String creatorFirstName;
+    private String creatorLastName;
+    private Date creationDate;
+    private Boolean isEdited;
+
+    /* Constructor */
+    public void Record() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int newId) {
+
+    }
+
+    public int getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(int newCreatorId) {
+
+    }
+
+    public String getCreatorFirstName() {
+        return creatorFirstName;
+    }
+
+    public void setCreatorFirstName(String newCreatorFirstName) {
+
+    }
+
+    public String getCreatorLastName() {
+        return creatorLastName;
+    }
+
+    public void setCreatorLastName(String newCreatorLastName) {
+
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String newCreationDate) {
+
+    }
+
+    public Boolean getIsEdited() {
+        return isEdited;
+    }
+
+    public void setIsEdited(String newIsEdited) {
+
+    }
+}
