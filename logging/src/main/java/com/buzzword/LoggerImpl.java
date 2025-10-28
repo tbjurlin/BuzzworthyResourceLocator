429deaed739f780b0aba21f8d43f7a722d746c9a
Ted Bjurlin <tbjurlin@rocketsoftware.com>
2025-10-28 14:35:54 -0400

    Implemented logger

--- logging/src/main/java/com/buzzword/LoggerImpl.java
+++ logging/src/main/java/com/buzzword/LoggerImpl.java
@@ -7,37 +7,32 @@ public class LoggerImpl implements Logger {
     private final org.apache.logging.log4j.Logger logger;
 
     public LoggerImpl(String loggerName) {
-        this.logger = LogManager.getLogger(loggerName);
+        logger = LogManager.getLogger(loggerName);
     }
 
     @Override
     public void trace(String message) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'trace'");
+        logger.trace("{}", message);
     }
 
     @Override
     public void debug(String message) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'debug'");
+        logger.debug("{}", message);
     }
 
     @Override
     public void info(String message) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'info'");
+        logger.info("{}", message);
     }
 
     @Override
     public void warn(String message) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'warn'");
+        logger.warn("{}", message);
     }
 
     @Override
     public void error(String message) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'error'");
+        logger.error("{}", message);
     }
     
 }
