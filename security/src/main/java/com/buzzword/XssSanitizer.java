dae3b4aeba2050ea41837739e5183a88b5696b96
Janniebth Melendez <JMelendeztorres@rocketsoftware.com>
2025-10-29 14:55:28 -0500

    Beginning of Security Information

---
+++ security/src/main/java/com/buzzword/XssSanitizer.java
@@ -0,0 +1,32 @@
+/**
+ * This is the utility interface to sanitize String against xss attacks.
+ * 
+ * @author Janniebeth Melendez
+ * @version 1.0
+ */
+package com.buzzword;
+
+
+public interface XssSanitizer {
+
+    /**
+     * Scan Routine
+     * 
+     * @param input string that we are scanning
+     * @return the clean/sanitized string
+     */
+    String sanitizeInput(String input);
+
+
+    /**
+     * Encode Routine
+     * 
+     * Sanitizes the ouput as a way to avoid unsafe HTML, as well as remove
+     * whitespace to ensure safe and clean content is displayed.
+     * 
+     * @param input
+     * @return
+     */
+    String sanitizeOutput(String input);
+
+}
