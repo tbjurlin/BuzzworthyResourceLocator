19683be9083e2b3d1b8972408e4e52e7a1a2d75a
Ted Bjurlin <tbjurlin@rocketsoftware.com>
2025-10-30 12:25:50 -0400

    Added doc comments

--- logging/src/main/java/com/buzzword/LoggerFactory.java
+++ logging/src/main/java/com/buzzword/LoggerFactory.java
@@ -1,10 +1,28 @@
 package com.buzzword;
 
+/**
+ * Factory class to provide access to Loggers.
+ * <p>
+ * Provides access to the implementations of the security and event loggers.
+ * 
+ * @author Ted Bjurlin
+ */
 public class LoggerFactory {
 
+    /**
+     * The logger instance that logs security events.
+     */
     private static Logger securityLogger;
+
+    /**
+     * The logger instance that logs application events.
+     */
     private static Logger eventLogger;    
 
+    /**
+     * Provides singleton access to the securiy logger.
+     * @return The logger instance for security.
+     */
     public static Logger getSecurityLogger() {
         if (securityLogger == null) {
             securityLogger = new LoggerImpl("com.buzzword.SecurityLogger");
@@ -12,6 +30,10 @@ public class LoggerFactory {
         return securityLogger;
     }
 
+    /**
+     * Provides singleton access to the event logger.
+     * @return The logger instance for application events.
+     */
     public static Logger getEventLogger() {
         if (eventLogger == null) {
             eventLogger = new LoggerImpl("com.buzzword.EventLogger");
