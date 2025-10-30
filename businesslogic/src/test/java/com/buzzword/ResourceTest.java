38dd25bec82b5e563181803bf29eb74812f502c3
Dennis Shelby <dshelby@rocketsoftware.com>
2025-10-30 17:19:19 -0400

    Abandoning validation annotations.

--- businesslogic/src/test/java/com/buzzword/ResourceTest.java
+++ businesslogic/src/test/java/com/buzzword/ResourceTest.java
@@ -9,11 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertThat;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
-
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 
 /**
@@ -21,12 +16,8 @@ import org.junit.jupiter.api.BeforeEach;
  */
 public class ResourceTest {
 
-    private Validator validator;
-
     @BeforeEach
     void setup() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
     }
 
 
@@ -34,33 +25,14 @@ public class ResourceTest {
     public void testInvalidId() {
         Resource resource = new Resource();
         resource.setId(-1);
-        Set<ConstraintViolation<Resource>> violations = validator.validate(resource);
-
-
-        assertFalse(violations.isEmpty());
-
-
-        resource.setCreatorId(-1);
-        resource.setCreatorFirstName("");
-
-       
-        // Valid Input
         
-        resource.setCreatorId(1);
-        resource.setCreatorFirstName("FirstName");
-
-        violations = validator.validate(resource);
-        assertTrue(violations.isEmpty());
     }
 
     @Test
     public void testValidId() {
         Resource resource = new Resource();
         resource.setId(1);
-        Set<ConstraintViolation<Resource>> violations = validator.validate(resource);
-        assertTrue(violations.isEmpty());
-        ConstraintViolation<Resource> violation = violations.iterator().next();
-        assertEquals("id must be non-negative", violation.getMessage());
+
     }
 }
 
