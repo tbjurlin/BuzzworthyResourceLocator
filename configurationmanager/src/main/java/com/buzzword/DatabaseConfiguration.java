47a5658ddf46c86e5a0adec407334bb603f9ba2a
Ted Bjurlin <tbjurlin@rocketsoftware.com>
2025-10-30 14:27:08 -0400

    Improved doc comments

--- configurationmanager/src/main/java/com/buzzword/DatabaseConfiguration.java
+++ configurationmanager/src/main/java/com/buzzword/DatabaseConfiguration.java
@@ -1,6 +1,16 @@
 package com.buzzword;
 
+/**
+ * The database configuration information.
+ * <p>
+ * Retreives information from the configuration files and constructs a database connection string.
+ * @author Ted Bjurlin
+ */
 public interface DatabaseConfiguration {
 
+    /**
+     * Getter for the database connection string for the current database implementation.
+     * @return database connection string
+     */
     public abstract String getDatabaseConnectionString();
 }
